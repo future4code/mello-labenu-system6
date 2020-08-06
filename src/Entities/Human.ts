149831e98857a42c2d94af8dc2b7639fc627cd44
Victor <victorgutierrezgomes@gmail.com>
2020-08-06 10:33:42 -0400

    Chore: implement Teacher

--- src/Entities/Human.ts
+++ src/Entities/Human.ts
@@ -2,10 +2,13 @@ import { IHumanEntity } from "../Types";
 import { v4 } from "uuid";
 
 export abstract class Human implements IHumanEntity {
+    id: string;
+
     constructor(
-        public nome: string,
-        public email: string,
-        public dataDeNascimento: string,
-        public id = v4()
-    ) {}
+        public readonly nome: string,
+        public readonly email: string,
+        public readonly dataDeNascimento: string
+    ) {
+        this.id = v4();
+    }
 }
---
+++ src/Entities/Teacher.ts
@@ -0,0 +1,13 @@
+import { ITeacher, ITeacherPerks } from "../Types/index";
+import { Human } from "./Human";
+
+export class Teacher extends Human implements ITeacher {
+    constructor(
+        public readonly nome: string,
+        public readonly email: string,
+        public readonly dataDeNascimento: string,
+        public perks: ITeacherPerks[]
+    ) {
+        super(nome, email, dataDeNascimento);
+    }
+}
